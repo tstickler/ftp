78c47e4ccf73c0e4e7812b2ac7b246748f3425b7
tyler <tstickler17@yahoo.com>
2017-05-11 09:50:20 -0700

    First commit

---
+++ cli.py
@@ -0,0 +1,62 @@
+from socket import *
+import sys
+import os
+
+# *******************************************************************
+# This file illustrates how to send a file using an
+# application-level protocol where the first 10 bytes
+# of the message from client to server contain the file
+# size and the rest contain the file data.
+# *******************************************************************
+import socket
+import os
+import sys
+import commands
+
+# Command line checks
+if len(sys.argv) < 3:
+    print "USAGE python " + sys.argv[0] + " <server_machine> <server_port>"
+
+# Server address
+serverAddr = sys.argv[1]
+
+# Server port
+serverPort = int(sys.argv[2])
+
+# Create a TCP socket
+connSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the server
+connSock.connect((serverAddr, serverPort))
+
+# Receive connection status from server
+reply = connSock.recv(1024)
+
+if reply == "Connection Accepted":
+    print reply, "\n"
+
+    user_in = ""
+    while user_in != "QUIT":
+        user_in = raw_input("ftp> ")
+
+        user_in = user_in.upper()
+
+        if user_in == "QUIT":
+            connSock.send("QUIT")
+        elif user_in == "GET":
+            connSock.send("GET")
+        elif user_in == "PUT":
+            connSock.send("PUT")
+        elif user_in == "LS":
+            connSock.send("LS")
+        elif user_in == "LLS":
+            # Run ls command, get output, and print it
+            for line in commands.getstatusoutput('ls -l'):
+                print line
+        else:
+            print "Invalid input"
+
+    print "Closing connection"
+    connSock.close()
+else:
+    print "Fail"
---
+++ serv.py
@@ -0,0 +1,53 @@
+# *****************************************************
+# This file implements a server for receiving the file
+# sent using sendfile(). The server receives a file and
+# prints it's contents.
+# *****************************************************
+
+import socket
+import os
+import sys
+import commands
+
+if len(sys.argv) < 2:
+    print "USAGE python " + sys.argv[0] + "<PORT NUMBER>"
+
+# The port on which to listen
+listen_port = int(sys.argv[1])
+
+# Create a welcome socket.
+welcomeSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the port
+welcomeSock.bind(('', listen_port))
+
+# Start listening on the socket
+welcomeSock.listen(1)
+
+# Accept connections forever
+while True:
+    print "Waiting for connections..."
+
+    # Accept connections
+    clientSock, addr = welcomeSock.accept()
+
+    print "Accepted connection from client: ", addr
+    print "\n"
+
+    clientSock.sendall("Connection Accepted")
+
+    # Keep client connection to allow multiple actions
+    while True:
+        # Receive user action
+        action = clientSock.recv(1024)
+
+        if action == "QUIT":
+            print "Client terminated connection"
+            clientSock.close()
+            break
+        elif action == "GET":
+            print "get"
+        elif action == "PUT":
+            print "put"
+        elif action == "LS":
+            print "ls"
